b85cca3f59d206e35a6e6f846886d93c9eb1fa89
41761707 <wojtczakov@gmail.com>
2022-10-22 21:36:25 +0200

    Poprawa kodu + testy parsera

--- Inzynierka/sources/graphplan.pl
+++ Inzynierka/sources/graphplan.pl
@@ -21,22 +21,22 @@ del(X,[Y | Tail], [Y|Tail1]) :-
 :-op(100,fx,~). %Negation of literal
 
 can(persist(P),[P]).
-can( go(R,A,B), [at(R,A), clear(B)]) :-
-    robot(R), adjacent(A,B).
+can(go(R,A,B), [at(R,A), clear(B)]) :-
+    robot(R), 
+    adjacent(A,B).
 
 
 effects(persist(P),[P]).
 effects(go(R,A,B), [at(R,B),clear(A),~at(R,A),~clear(B)]).
 
-robot(a).
+robot(a). robot(b). robot(c).
 
 adjacent(A,B) :-
     n(A,B)
     ;
     n(B,A).
 
-n(1,2).
-n(2,1).
+n(1,2). n(2,3). n(4,5). n(5,6). n(1,4). n(2,5). n(3,6).
 
 incosistent(G,~G).
 incosistent(~G,G).
@@ -49,7 +49,7 @@ inconsistent(at(R1,C),at(R2,C)) :-
     R1 \== R2.
 
 
-state0( [at(a,1),clear(2)]).
+state0([at(a,1), at(b,2), at(c,3), clear(4), clear(5), clear(6)]).
 
 
 
@@ -59,13 +59,25 @@ plan(StartState, Goals, Plan) :-
     findall(P/1, member(P,StartState),StartLevel),  %Start level of planning graph
     setof(action(A, PreCond, Effects), (effects(A,Effects),can(A,PreCond)),AllActions),
         %AllActions is a list of all avaiable actions in the planning domain
+    write("StartLevel: "), write(StartLevel), nl,
+    write("Goals: "), write(Goals), nl,
+    write("Plan: "), write(Plan), nl,
+    write("AllActions: "), write(AllActions), nl,
     graphplan([StartLevel], Goals, Plan, AllActions).
 
 graphplan([StateLevel | PlanGraph], Goals, Plan, AllActs) :-
+    write("StateLevel "), write(StateLevel), nl,
     satisfied(StateLevel, Goals),   %Goals true in StateLevel
     extract_plan([StateLevel | PlanGraph], Plan)
+    ,write("Plan: "), write(Plan), nl
     ;
     expand(StateLevel, ActionLevel, NewStateLev, AllActs), %Generate new action and state levels
+    write("StateLevel: "), write(StateLevel), nl,
+    write("ActionLevel: "), write(ActionLevel), nl,
+    write("NewStateLev: "), write(NewStateLev), nl,
+    write("AllActs: "), write(AllActs), nl,
+    length(StateLevel,X),
+    X>0,
     graphplan([NewStateLev, ActionLevel, StateLevel | PlanGraph], Goals, Plan, AllActs).
 
 %satisfied(StateLevel, Goals): Goals are true in StateLevel
@@ -96,12 +108,16 @@ extract_plan([_,ActionLevel | RestOfGraph], Plan) :-
     %performed on StateLevel to result in NextStateLevel
 
 expand(StateLev, ActionLev, NextStateLev, AllActs) :-
+    writeln("Inside expand"),
     add_actions(StateLev, AllActs, [], ActionLev1, [], NextStateLev1),
     findall(action(persist(P),[P],[P]),member(P/_,StateLev),PersistActs),
         %All persist actions for StateLev
     add_actions(StateLev, PersistActs, ActionLev1, ActionLev, NextStateLev1, NextStateLev),
     mutex_constr(ActionLev),    %Set up mutex constraints between actions
-    mutex_constr(NextStateLev). %Set up mutex constraints between literals
+    mutex_constr(NextStateLev), %Set up mutex constraints between literals
+    write("AllActs: "), write(AllActs), nl,
+    write("ActionLev1: "), write(ActionLev1), nl,
+    write("NextStateLev1: "), write(NextStateLev1), nl.
 
 %add_actions(StateLev, Acts, ActLev0, ActLev, NextStateLev0, NextStateLev):
     %ActLev = ActLev0 + Acts that are potentially possible in StateLev
@@ -111,9 +127,10 @@ expand(StateLev, ActionLev, NextStateLev, AllActs) :-
 add_actions(_,[],ActionLev, ActionLev, NextStateLev, NextStateLev).
 
 add_actions(StateLev, [action(A,PreCond,Effects) | Acts], ActLev0, ActLev, NextLev0, NextLev) :-
+    write("Inside add_actions"), nl,
     TA in 0..1,     %Boolean indicator variable for action A
     includes(StateLev, PreCond, TA),    %A is potentially possible in StateLev
-    add_effects(TA, Effects, NextLev0, NextLev1), !,
+    add_effects(TA, Effects, NextLev0, NextLev1), !, 
     add_actions(StateLev, Acts, [A/TA | ActLev0], ActLev, NextLev1, NextLev)
     ;
     add_actions(StateLev, Acts, ActLev0, ActLev, NextLev0, NextLev).
@@ -124,8 +141,11 @@ add_actions(StateLev, [action(A,PreCond,Effects) | Acts], ActLev0, ActLev, NextL
 includes(_,[],_).
 
 includes(StateLev, [P|Ps],TA) :-
+    write("Inside includes"), nl,
+    write("TA: "), write(TA), nl,
     member(P/T, StateLev),
-    TA #< T,    %If action A occurs the P must be true
+    write("T: "), write(T), nl,
+    TA #=< T,    %If action A occurs the P must be true
     includes(StateLev, Ps, TA).
 
 %add_effects(TA,Effects,StateLev,ExpandedStateLev):
@@ -135,8 +155,10 @@ includes(StateLev, [P|Ps],TA) :-
 add_effects(_,[],StateLev,StateLev).
 
 add_effects(TA, [P | Ps], StateLev0, ExpandedState) :-
+    write("Inside add_effects"), nl,
     (del(P/TP,StateLev0,StateLev1), !,
     NewTP #= TP+TA,
+    %write("NewTP: "), write(NewTP), nl,
     StateLev = [P/NewTP | StateLev1]    %Add support of action for P 
     ;
     StateLev = [P/TA | StateLev0], !    %Truth of P equivalent to TA
@@ -194,11 +216,35 @@ collect_vars([X/V | Rest], Vars) :-
     (X \= persist(_), var(V), !, Vars= [V | RestVars] % include V if V is "non-persist" variable
     ;
     Vars = RestVars),
-    collect_vars(Rest,RestVars).    %Collect rest of variables 
+    collect_vars(Rest,RestVars).    %Collect rest of variables
 
 
+%PARSER OUT
 
+%TEST PARSER
 
+%[[go(a, 1, 4)], 
+%[go(b, 2, 1), go(a, 4, 5)], 
+%[go(c, 3, 2), go(b, 1, 4), go(a, 5, 6)], 
+%[go(c, 2, 1), go(a, 6, 3)]] .
+
+parser_out([],_).
+
+parser_out([CurrentLevel | RestOfPlan ],Step) :-
+    write("Krok: "), writeln(Step),
+    parse(CurrentLevel),
+    NextStep is Step+1,
+    parser_out(RestOfPlan,NextStep).
+
+parse([]).
+
+parse([Current | RestOfLevel]) :-
+    Current =..[Function,Block,From,To],
+    write("Funktor: "), write(Function), write(" "),
+    write("Robot: "), write(Block), write(" "),
+    write("Z: "), write(From), write(" "),
+    write("Do: "), write(To), writeln(" "),
+    parse(RestOfLevel).
 
 
 
