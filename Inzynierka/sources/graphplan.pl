68cd9cba0e65bca45c3046219b96b11b1c0d5d6e
41761707 <wojtczakov@gmail.com>
2022-10-29 22:31:42 +0200

    Poprawki graphviz

--- Inzynierka/sources/graphplan.pl
+++ Inzynierka/sources/graphplan.pl
@@ -1,23 +1,10 @@
-%GRAPHPLAN wersja 1.0
+%GRAPHPLAN wersja 1.1
 
 %Przed zadaniem pytania należy dodatkowo uruchomić następującą procedurę:
 %use_module(library(clpfd)). <- Programowanie ograniczeń
 %testowe wywołanie:
 %state0(S), plan(S,[at(a,3),at(c,1)],Plan).
 %state0(S), plan(S,[clear(1), at(a,2)],Plan).
-
-%Representation of a planning graph
-
-% PlanningGraph = [StateLevN,ActionLevN,StateLEvN1,...] where N1=N-1
-% Level N is the most recently generated level
-% StateLevel = [L1/TL1,L2/TL2,...] TL1, TL2,... are Boolean indticator variables of L1,L2,...
-% Action Level =[A1/TA1,...] TA1 is a Boolean indicator variable of A1
-
-%use_module(library(clpfd)).
-
-del(X,[X | Tail], Tail).
-del(X,[Y | Tail], [Y|Tail1]) :-
-    del(X,Tail,Tail1).
 
 :-op(100,fx,~). %Negation of literal
 
@@ -30,17 +17,16 @@ can(go(R,A,B), [at(R,A), clear(B)]) :-
 effects(persist(P),[P]).
 effects(go(R,A,B), [at(R,B),clear(A),~at(R,A),~clear(B)]).
 
-robot(a). %robot(b). robot(c).
+%robot(a). robot(b). robot(c).
+robot(a).
 
 adjacent(A,B) :-
     n(A,B)
     ;
     n(B,A).
 
-n(1,2).
-n(2,1).
-%n(2,3).
-%n(3,2). 
+%n(1,2). n(2,3). n(4,5). n(5,6). n(1,4). n(2,5). n(3,6).
+n(1,2). n(2,1). n(2,3). n(3,2).
 
 incosistent(G,~G).
 incosistent(~G,G).
@@ -53,9 +39,12 @@ inconsistent(at(R1,C),at(R2,C)) :-
     R1 \== R2.
 
 
-state0([at(a,1), clear(2)]).
+%state0([at(a,1), at(b,2), at(c,3), clear(4), clear(5), clear(6)]).
+state0([at(a,1),clear(2),clear(3)]).
 
-
+remove(X,[X | Tail], Tail).
+remove(X,[Y | Tail], [Y|Tail1]) :-
+    remove(X,Tail,Tail1).
 
 %plan(StartState, Goals, Plan)
 
@@ -100,7 +89,8 @@ extract_plan([_],[]).   %Just single state level, no-action plan
 extract_plan([_,ActionLevel | RestOfGraph], Plan) :-
     collect_vars(ActionLevel, AVars),
     labeling([],AVars),
-    findall(A,(member(A/1,ActionLevel),A \= persist(_)), Actions),  %Actions to be executed
+    %findall(A,(member(A/1,ActionLevel),A \= persist(_)), Actions),  %Actions to be executed
+    findall(A, (member(A/1,ActionLevel)),Actions),
     extract_plan(RestOfGraph, RestOfPlan),
     append(RestOfPlan, [Actions], Plan).
 
@@ -120,8 +110,8 @@ expand(StateLev, ActionLev, NextStateLev, AllActs) :-
     mutex_constr(ActionLev),    %Set up mutex constraints between actions
     mutex_constr(NextStateLev), %Set up mutex constraints between literals
     %write("AllActs: "), write(AllActs), nl,
-    write("ActionLevel: "), write(ActionLev1), nl,
-    write("StateLevel: "), write(NextStateLev1), nl.
+    write("ActionLevel: "), write(ActionLev),nl,
+    write("StateLevel: "), write(NextStateLev), nl.
 
 %add_actions(StateLev, Acts, ActLev0, ActLev, NextStateLev0, NextStateLev):
     %ActLev = ActLev0 + Acts that are potentially possible in StateLev
@@ -160,7 +150,7 @@ add_effects(_,[],StateLev,StateLev).
 
 add_effects(TA, [P | Ps], StateLev0, ExpandedState) :-
     %write("Inside add_effects"), nl,
-    (del(P/TP,StateLev0,StateLev1), !,
+    (remove(P/TP,StateLev0,StateLev1),!,
     NewTP #= TP+TA,
     %write("NewTP: "), write(NewTP), nl,
     StateLev = [P/NewTP | StateLev1]    %Add support of action for P 
@@ -193,12 +183,16 @@ mutex_constr1(P/T, [P1/T1 | Rest]) :-
     ),
     mutex_constr1(P/T,Rest).
 
-mutex(P,~P) :- !.   %Negated conditions are always mutually exclusive 
+mutex(P,~P) :- 
+    write("Mutex: ["), write(P), write(","),write(~P), writeln("]"),!.   %Negated conditions are always mutually exclusive 
 
-mutex(~P,P) :- !.  
+mutex(~P,P) :- 
+    write("Mutex: ["), write(~P), write(","),write(P), writeln("]"),!.  
 
 mutex(A,B) :-               %Domain-specific pair of inconsistent literals or actions
-    inconsistent(A,B), !.   %Defined by the user in planning domain definition
+    inconsistent(A,B),
+    write("Mutex: ["), write(A), write(","),write(B), writeln("]"), 
+    !.   %Defined by the user in planning domain definition
 
 %Actions A1, A2 mutually exclusive due to interference
 %that is action A1 effect is incosistent with A2 precondition or vice versa
--- Inzynierka/sources/parsePlan.py
+++ Inzynierka/sources/parsePlan.py
@@ -1,6 +1,7 @@
 import sys
 import graphviz
 import re
+import itertools
 
 
 def generateArcs(actions,g,level):
@@ -14,21 +15,36 @@ def generateArcs(actions,g,level):
             else:
                 Effects = action_variables[variable]
         for item in PreCond:
-            pass
-            g.edge(item+str(level),action_id+str(level))
+            if(action_id.startswith("persist")):
+                g.edge(item+str(level),action_id+str(level),style='dashed')
+            else:
+                g.edge(item+str(level),action_id+str(level))
         for item in Effects:
-            pass
-            g.edge(action_id+str(level),item+str(level_higher))
+            if(action_id.startswith("persist")):
+                g.edge(action_id+str(level),item+str(level_higher),style='dashed')
+            else:
+                g.edge(action_id+str(level),item+str(level_higher))
+
+def generateMutexArcs(variables,g,level):
+    #print(variables)
+    for i,pair in enumerate(variables):
+        new_pair = [pair[1],pair[0]]
+        if new_pair not in variables[i:]:
+            g.edge(pair[0]+str(level),pair[1]+str(level), style='dotted',arrowhead='none',constraint='false')
+        
+        #g.edge(pair[0]+str(level),pair[1]+str(level), color='lightblue',arrowhead='none',constraint='false')
 
 
-def generateLayer(name,variables,g,level):
+def generateLayer(name,variables,g,level,type):
     layer_name = 'cluster_' + name
     with g.subgraph(name=layer_name) as layer:
+        #layer.attr(color='white')
         layer.attr(color='gray')
         layer.attr(label=name)
+        #layer.attr('node', shape='box')
         for item in variables:
-            print(item)
-            layer.node(item+str(level),item)
+            #print(item)
+            layer.node(item+str(level),item, shape=type)
     
 
 def readInput(filename):
@@ -43,6 +59,7 @@ def parseInput(file,g):
     current_level = 1
     current_level_actions = {}
     used_actions = {}
+    mutex_states = []
     prev_action = ''
     for item in file:
         name,variables = item.split(':')
@@ -50,7 +67,7 @@ def parseInput(file,g):
             name = 'StateLevel1'
             #print(variables)
             variables = re.findall('~?at\([a-z]+,[0-9]+\)|~?go\([a-z]+,[0-9]+,[0-9]+\)|~?clear\([0-9]+\)',variables)
-            generateLayer(name,variables,g,current_level)
+            generateLayer(name,variables,g,current_level,"oval")
         elif(name == 'A'):
             prev_action = variables.strip()
             current_level_actions[prev_action] = {}
@@ -63,14 +80,20 @@ def parseInput(file,g):
             variables = re.findall('~?at\([a-z]+,[0-9]+\)|~?go\([a-z]+,[0-9]+,[0-9]+\)|~?clear\([0-9]+\)',variables)
             current_level_actions[prev_action]['Effects'] = variables
             #print('Effects: ',variables)
+        elif(name == 'Mutex'):
+            variables = re.findall('~?at\([a-z]+,[0-9]+\)|~?go\([a-z]+,[0-9]+,[0-9]+\)|~?clear\([0-9]+\)',variables)
+            mutex_states.append(variables)
         elif(name == 'ActionLevel'):
-            variables = re.findall('go\([a-z]+,[0-9]+,[0-9]+\)|~?persist\(at\([a-z]+,[0-9]+\)\)',variables)
-            generateLayer(name+str(current_level),variables,g,current_level)
+            mutex_states.sort()
+            generateMutexArcs(list(mutex_states for mutex_states,_ in itertools.groupby(mutex_states)),g,current_level+1)
+            mutex_states = []
+            variables = re.findall('go\([a-z]+,[0-9]+,[0-9]+\)|persist\(~?at\([a-z]+,[0-9]+\)\)|persist\(~?clear\([0-9]+\)\)',variables)
+            generateLayer(name+str(current_level),variables,g,current_level,"box")
         elif(name == 'StateLevel'):
             current_level = current_level + 1
             variables = re.findall('~?at\([a-z]+,[0-9]+\)|~?go\([a-z]+,[0-9]+,[0-9]+\)|~?clear\([0-9]+\)',variables)
-            generateLayer(name+str(current_level),variables,g,current_level)
-            print(current_level_actions)
+            generateLayer(name+str(current_level),variables,g,current_level,"oval")
+            #print(current_level_actions)
             #used_actions = {k: current_level_actions[k] for k in variables}
             #current_level_actions.clear()
             #generateArcs(used_actions,g,current_level-1)
@@ -86,6 +109,8 @@ def main():
     file = readInput(sys.argv[1])
     parseInput(file,g)
     g.attr(rankdir='LR')
+
+    
     g.view()
 
 if __name__=='__main__':
